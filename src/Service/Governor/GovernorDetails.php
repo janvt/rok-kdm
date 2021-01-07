4158300010053a15bac9f5a694a3b80ef64d9e06
janvt <janvt@me.com>
2021-01-08 00:44:19 +0100

    add more data to details page

--- src/Service/Governor/GovernorDetails.php
+++ src/Service/Governor/GovernorDetails.php
@@ -15,10 +15,16 @@ class GovernorDetails
     public $alliance;
     public $power;
     public $highestPower;
-    public $kills;
     public $deads;
     public $helps;
     public $rssAssistance;
+
+    public $kills;
+    public $t1kills;
+    public $t2kills;
+    public $t3kills;
+    public $t4kills;
+    public $t5kills;
 
     public $status;
     public $displayStatus;
@@ -27,15 +33,23 @@ class GovernorDetails
     {
         $this->id = $gov->getGovernorId();
         $this->name = $gov->getName();
+        $this->alliance = $gov->getAlliance();
+
         $this->status = $gov->getStatus();
         $this->displayStatus = GovernorStatus::getDisplayStatus($gov->getStatus());
-        $this->alliance = $gov->getAlliance();
+
         $this->power = $mergedSnapshot->getPower();
         $this->highestPower = $mergedSnapshot->getHighestPower();
-        $this->kills = $this->sumKills($mergedSnapshot);
         $this->deads = $mergedSnapshot->getDeads();
         $this->helps = $mergedSnapshot->getHelps();
         $this->rssAssistance = $mergedSnapshot->getRssAssistance();
+
+        $this->kills = $this->sumKills($mergedSnapshot);
+        $this->t1kills = $mergedSnapshot->getT1Kills();
+        $this->t2kills = $mergedSnapshot->getT2Kills();
+        $this->t3kills = $mergedSnapshot->getT3Kills();
+        $this->t4kills = $mergedSnapshot->getT4Kills();
+        $this->t5kills = $mergedSnapshot->getT5Kills();
     }
 
     private function sumKills(GovernorSnapshot $snapshot): int
