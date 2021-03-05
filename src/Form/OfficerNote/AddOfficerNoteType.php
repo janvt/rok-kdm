72891a819670ca34b13330db5ade92bb63f21761
janvt <janvt@me.com>
2021-03-05 19:20:28 +0100

    form and layout improvements

--- src/Form/OfficerNote/AddOfficerNoteType.php
+++ src/Form/OfficerNote/AddOfficerNoteType.php
@@ -5,6 +5,7 @@ namespace App\Form\OfficerNote;
 use App\Entity\OfficerNote;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class AddOfficerNoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('tldr')
+            ->add('tldr', TextType::class, ['label' => 'TLDR'])
             ->add('note')
             ->add('submit', SubmitType::class)
         ;
